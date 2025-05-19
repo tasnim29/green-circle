aadf8758c8928a743942c98511ac94feb4447068
tasnim29 <tasnimmahmudnjr@gmail.com>
2025-05-19 23:47:05 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+import React from "react";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
